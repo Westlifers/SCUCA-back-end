c1d6a87fe0cd1c620e32dd698e6f3a1a980554cb
Westlifers <2585687459@qq.com>
2022-03-22 22:32:43 +0800

    优化competition接口数据，但没有完全优化，交给前端吧

--- app/serializers.py
+++ app/serializers.py
@@ -1,12 +1,15 @@
-from numpy import source
 from rest_framework import serializers
 from .models import *
 
 
-class CompetitionSerializer(serializers.ModelSerializer):
+""" class CompetitionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Competition
-        fields = ['date', 'item_222', 'item_333', 'item_444', 'item_555', 'item_666', 'item_777']
+        fields = ['date', 'item_222', 'item_333', 'item_444', 'item_555', 'item_666', 'item_777', 'result']
+    
+    result = serializers.JSONField(source='result_set.all')
+    def get_results(self, obj):
+        return obj.result_set.all() """
 
 
 class ResultSerializer(serializers.ModelSerializer):
@@ -17,3 +20,10 @@ class ResultSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username')
     def get_username(self, obj):
         return obj.user.username
+
+class CompetitionSerializer(serializers.ModelSerializer):
+    result_set = ResultSerializer(many=True)
+    print(result_set)
+    class Meta:
+        model = Competition
+        fields = '__all__'
